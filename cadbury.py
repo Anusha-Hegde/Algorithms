2bff0a0209023ced34bd2b5680dd5eb162655983
Anusha-Hegde <anushaprhegde@gmail.com>
2020-07-19 15:51:29 +0530

    Dole out Cadbury

---
+++ cadbury.py
@@ -0,0 +1,62 @@
+'''
+Dole Out Cadbury
+
+You are a teacher in reputed school. During Celebration Day you were assigned a task to distribute Cadbury such that maximum children get the chocolate. 
+You have a box full of Cadbury with different width and height. You can only distribute largest square shape Cadbury. 
+So if you have a Cadbury of length 10 and width 5, then you need to break Cadbury in 5X5 square and distribute to first child and then remaining 5X5 to next in queue.
+
+Input Format
+
+First line contains an integer P that denotes minimum length of Cadbury in the box
+
+Second line contains an integer Q that denotes maximum length of Cadbury in the box
+
+Third line contains an integer R that denotes minimum width of Cadbury in the box
+
+Fourth line contains an integer S that denotes maximum width of Cadbury in the box
+
+Output
+
+Print total number of children who will get chocolate.
+
+Explanation
+
+Example 1
+
+Input
+
+5
+
+7
+
+3
+
+4
+
+Output
+
+24
+
+Explanation
+
+Length is in between 5 to 7 and width is in between 3 to 4.
+
+So we have 5X3,5X4,6X3,6X4,7X3,7X4 type of Cadbury in the box.
+
+If we take 5X3 :
+
+First, we can give 3X3 square Cadbury to 1st child .Then we are left with 3X2. Now largest square is 2X2 which will be given to next child. Next, we are left with two  1X1 part of Cadbury which will be given to another two children.
+'''
+
+a, b, c, d = list(map(int, input().split(' ')))
+count = 0
+for i in range(a, b+1):
+    for j in range(c, d+1):
+        m, n = i, j
+        while n > 1:
+            x = m - n
+            count += 1
+            m = max(n, x)
+            n = min(n, x)
+        count += (n * m)
+print(count)
